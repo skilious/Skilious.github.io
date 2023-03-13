d0fb500bc8622078dd7103c60d44fde79f869472
Quoc Ha <QuocHa@protonmail.com>
2023-03-13 20:07:18 +1100

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -1,184 +1,196 @@
 // gitprofile.config.js
 
 const config = {
-  github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
-    sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
-    exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+    github: {
+        username: 'Skilious', // Your GitHub org/user name. (Required)
+        sortBy: 'updated', // stars | updated
+        limit: 10, // How many projects to display.
+        exclude: {
+            forks: true, // Forked projects will not be displayed if set to true.
+            projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+        },
     },
-  },
-  social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    facebook: '',
-    instagram: '',
-    dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // format: userid/username
-    skype: '',
-    telegram: '',
-    website: 'https://arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
-  },
-  resume: {
-    fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
-  },
-  skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
-  ],
-  experiences: [
-    {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+    social: {
+        linkedin: 'quoc-ha',
+        twitter: '',
+        mastodon: '',
+        facebook: '',
+        instagram: '',
+        dribbble: '',
+        behance: '',
+        medium: '',
+        dev: '',
+        stackoverflow: '', // format: userid/username
+        skype: '',
+        telegram: '',
+        website: '',
+        phone: '',
+        email: 'quocha@protonmail.com',
     },
-    {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+    resume: {
+        fileUrl:
+            'https://drive.google.com/file/d/14nX5juiKW7MJp_B1hVzKmwWy4vmz-Ecw/view?usp=share_link', // Empty fileUrl will hide the `Download Resume` button.
     },
-  ],
-  /* certifications: [
-    {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com'
-    },
-  ], */
-  education: [
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
-    },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
-    },
-  ],
-
-  // To hide the `My Projects` section, keep it empty.
-  externalProjects: [
-    {
-      title: 'Project Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
-    },
-    {
-      title: 'Project Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
-    },
-  ],
-  // Display blog posts from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
-  },
-  googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-  },
-  // Track visitor interaction and behavior. https://www.hotjar.com
-  hotjar: {
-    id: '',
-    snippetVersion: 6,
-  },
-  themeConfig: {
-    defaultTheme: 'winter',
-
-    // Hides the switch in the navbar
-    // Useful if you want to support a single color mode
-    disableSwitch: false,
-
-    // Should use the prefers-color-scheme media-query,
-    // using user system preferences, instead of the hardcoded defaultTheme
-    respectPrefersColorScheme: false,
-
-    // Hide the ring in Profile picture
-    hideAvatarRing: false,
-
-    // Available themes. To remove any theme, exclude from here.
-    themes: [
-      'light',
-      'dark',
-      'cupcake',
-      'bumblebee',
-      'emerald',
-      'corporate',
-      'synthwave',
-      'retro',
-      'cyberpunk',
-      'valentine',
-      'halloween',
-      'garden',
-      'forest',
-      'aqua',
-      'lofi',
-      'pastel',
-      'fantasy',
-      'wireframe',
-      'black',
-      'luxury',
-      'dracula',
-      'cmyk',
-      'autumn',
-      'business',
-      'acid',
-      'lemonade',
-      'night',
-      'coffee',
-      'winter',
-      'procyon',
+    skills: [
+        'C++',
+        'C#',
+        'Unity',
+        'Unreal Engine 4',
+        'Networking Unreal Engine 4',
+        'Github Version Control',
+        'Java',
+        'Python',
+        'SQL',
+        'Game Development',
+        'Game Design',
+        'Adobe Photoshop'
+    ],
+    experiences: [
+        {
+            company: '',
+            position: '',
+            from: ' ',
+            to: '',
+            companyLink: '',
+        },
+        {
+            company: '',
+            position: '',
+            from: '',
+            to: '',
+            companyLink: '',
+        },
+    ],
+    /* certifications: [
+      {
+        name: 'Lorem ipsum',
+        body: 'Lorem ipsum dolor sit amet',
+        year: 'March 2022',
+        link: 'https://example.com'
+      },
+    ], */
+    education: [
+        {
+            institution: 'Torrens University Australia',
+            degree: 'Bachelor of Software Engineering - Game Programming',
+            from: '2019',
+            to: '2021'
+        },
+        {
+            institution: 'Torrens University Australia',
+            degree: 'Diploma of Design',
+            from: '2018',
+            to: '2018'
+        },
     ],
 
-    // Custom theme
-    customTheme: {
-      primary: '#fc055b',
-      secondary: '#219aaf',
-      accent: '#e8d03a',
-      neutral: '#2A2730',
-      'base-100': '#E3E3ED',
-      '--rounded-box': '3rem',
-      '--rounded-btn': '3rem',
+    // To hide the `My Projects` section, keep it empty.
+    externalProjects: [
+        {
+            title: 'Unreal Engine 4 Major Project - Sigil, Lead Programmer',
+            description:
+                'Stylised 3D Survival Open Island RPG',
+            imageUrl: 'https://via.placeholder.com/250x250',
+            link: 'https://dayna-aspite.itch.io/sigil',
+        },
+        {
+            title: 'Unity 3D Playstation Port - Light Souls, Lead Programmer',
+            description:
+                'Top-Down Bullet Hell Inspired by Dark Souls',
+            imageUrl: 'https://via.placeholder.com/250x250',
+            link: 'https://skilious.itch.io/light-souls',
+        },
+        {
+            title: 'Unity 2D Rapid Prototype - Vanish, Game Developer',
+            description:
+                'Defeat Bacteria with Vanish Spray bottle!',
+            imageUrl: 'https://via.placeholder.com/250x250',
+            link: 'https://skilious.itch.io/vanish',
+        },
+        {
+            title: 'Unity 2D - Mobius, Gameplay Programmer',
+            description:
+                'Sci-fi Infinite Runner',
+            imageUrl: 'https://via.placeholder.com/250x250',
+            link: 'https://skilious.itch.io/mobius',
+        },
+    ],
+    // Display blog posts from your medium or dev account. (Optional)
+    blog: {
+        source: '', // medium | dev
+        username: '', // to hide blog section, keep it empty
+        limit: 2, // How many posts to display. Max is 10.
     },
-  },
+    googleAnalytics: {
+        id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    },
+    // Track visitor interaction and behavior. https://www.hotjar.com
+    hotjar: {
+        id: '',
+        snippetVersion: 6,
+    },
+    themeConfig: {
+        defaultTheme: 'winter',
 
-  // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
+        // Hides the switch in the navbar
+        // Useful if you want to support a single color mode
+        disableSwitch: false,
+
+        // Should use the prefers-color-scheme media-query,
+        // using user system preferences, instead of the hardcoded defaultTheme
+        respectPrefersColorScheme: false,
+
+        // Hide the ring in Profile picture
+        hideAvatarRing: false,
+
+        // Available themes. To remove any theme, exclude from here.
+        themes: [
+            'light',
+            'dark',
+            'cupcake',
+            'bumblebee',
+            'emerald',
+            'corporate',
+            'synthwave',
+            'retro',
+            'cyberpunk',
+            'valentine',
+            'halloween',
+            'garden',
+            'forest',
+            'aqua',
+            'lofi',
+            'pastel',
+            'fantasy',
+            'wireframe',
+            'black',
+            'luxury',
+            'dracula',
+            'cmyk',
+            'autumn',
+            'business',
+            'acid',
+            'lemonade',
+            'night',
+            'coffee',
+            'winter',
+            'procyon',
+        ],
+
+        // Custom theme
+        customTheme: {
+            primary: '#fc055b',
+            secondary: '#219aaf',
+            accent: '#e8d03a',
+            neutral: '#2A2730',
+            'base-100': '#E3E3ED',
+            '--rounded-box': '3rem',
+            '--rounded-btn': '3rem',
+        },
+    },
+
+    // Optional Footer. Supports plain text or HTML.
+    footer: `Made with <a 
       class="text-primary" href="https://github.com/arifszn/gitprofile"
       target="_blank"
       rel="noreferrer"
